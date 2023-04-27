1d20701cceac90814f96c70d157191dd16ac419c
Justin P <Jpartain89@gmail.com>
2023-04-27 04:58:10 -0500

    Update vulners.py
    Fixed spelling of "results" from "resukts"

--- plugins/modules/vulners.py
+++ plugins/modules/vulners.py
@@ -14,11 +14,11 @@ DOCUMENTATION = """
     description: >
          This plugins allows you to check check whether you have vulnerable packages installed on the target system.
           It requires a valid vulners API key to run: you can specify it as a CLI argument or store it in a file and specify its name as CLI argument.
-          The resukts are stored as json and html files under /tmp directory.
+          The results are stored as json and html files under /tmp directory.
     author: gmedian
     options:
         vulners_api_key:
-            description: You key for Vulners API (obtain one at https://vulners.com/)
+            description: Your key for the Vulners API (obtain one at https://vulners.com/)
             vars:
               - name: api_key
         vulners_api_key_file:
